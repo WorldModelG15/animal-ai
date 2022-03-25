c3abdbb316059bd05776eacb823cfcd99847beff
shibukazu <shibukazu@hotmail.co.jp>
2022-03-25 23:55:37 +0000

    save: 一時保存

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
